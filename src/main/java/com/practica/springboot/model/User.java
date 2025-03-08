5a48a88a9ecd3f46efba33df58b2bc54e239743f
Samuel <samuelbercian1706@gmail.com>
2025-03-08 08:43:31 -0600

    Actualizacion

--- src/main/java/com/practica/springboot/model/User.java
+++ src/main/java/com/practica/springboot/model/User.java
@@ -5,6 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -27,28 +35,4 @@ public class User {
 
     @Column(name = "password")
     private String password;
-    
-
-    //Constructor
-    public User() {
-    }
-
-    //GET and SET
-    public Integer getId(){return id;}
-    public void setId(Integer id){this.id = id;}
-
-    public String getName(){return name;}
-    public void setName(String name){this.name = name;}
-
-    public String getLastname(){return lastname;}
-    public void setLastname(String lastname){this.lastname = lastname;}
-
-    public int getAge(){return age;}
-    public void setAge(int age){this.age = age;}
-
-    public String getEmail(){return email;}
-    public void setEmail(String email){this.email = email;}
-
-    public String getPassword(){return password;}
-    public void setPassword(String password){this.password = password;}
 }
